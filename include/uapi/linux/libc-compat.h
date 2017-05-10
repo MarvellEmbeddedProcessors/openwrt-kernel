2e9fe170e0254c3b73d48cbe08dd131ef269331e
Marko Ratkaj <marko.ratkaj@sartura.hr>
2017-05-10 14:48:43 +0200

    uapi if_ether.h prevent ethhdr redefinition
    Signed-off-by: Marko Ratkaj <marko.ratkaj@sartura.hr>

--- include/uapi/linux/libc-compat.h
+++ include/uapi/linux/libc-compat.h
@@ -48,6 +48,14 @@
 #ifndef _UAPI_LIBC_COMPAT_H
 #define _UAPI_LIBC_COMPAT_H
 
+/* musl defines the ethhdr struct itself in its netinet/if_ether.h.
+ * Glibc just includes the kernel header and uses a different guard. */
+#if defined(_NETINET_IF_ETHER_H)
+#define __UAPI_DEF_ETHHDR		0
+#else
+#define __UAPI_DEF_ETHHDR		1
+#endif
+
 /* We have included glibc headers... */
 #if defined(__GLIBC__)
 
