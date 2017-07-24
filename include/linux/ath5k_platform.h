a5121a3ff300250f3f4d19e07dc422873db693c2
Damir Samardzic <damir.samardzic@sartura.hr>
2017-07-24 12:14:07 +0200

    include/linux: add required header files for compat-wireless

--- include/linux/ath5k_platform.h
+++ include/linux/ath5k_platform.h
@@ -2,6 +2,7 @@
  * Copyright (c) 2008 Atheros Communications Inc.
  * Copyright (c) 2009 Gabor Juhos <juhosg@openwrt.org>
  * Copyright (c) 2009 Imre Kaloz <kaloz@openwrt.org>
+ * Copyright (c) 2010 Daniel Golle <daniel.golle@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -16,44 +17,14 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef _LINUX_ATH9K_PLATFORM_H
-#define _LINUX_ATH9K_PLATFORM_H
+#ifndef _LINUX_ATH5K_PLATFORM_H
+#define _LINUX_ATH5K_PLATFORM_H
 
-#define ATH9K_PLAT_EEP_MAX_WORDS	2048
+#define ATH5K_PLAT_EEP_MAX_WORDS	2048
 
-struct ath9k_platform_data {
-	const char *eeprom_name;
-
-	u16 eeprom_data[ATH9K_PLAT_EEP_MAX_WORDS];
+struct ath5k_platform_data {
+	u16 *eeprom_data;
 	u8 *macaddr;
-
-	int led_pin;
-	u32 gpio_mask;
-	u32 gpio_val;
-
-	u32 bt_active_pin;
-	u32 bt_priority_pin;
-	u32 wlan_active_pin;
-
-	bool endian_check;
-	bool is_clk_25mhz;
-	bool tx_gain_buffalo;
-	bool disable_2ghz;
-	bool disable_5ghz;
-	bool led_active_high;
-
-	int (*get_mac_revision)(void);
-	int (*external_reset)(void);
-
-	bool use_eeprom;
-
-	int num_leds;
-	const struct gpio_led *leds;
-	const char *led_name;
-
-	unsigned num_btns;
-	const struct gpio_keys_button *btns;
-	unsigned btn_poll_interval;
 };
 
-#endif /* _LINUX_ATH9K_PLATFORM_H */
+#endif /* _LINUX_ATH5K_PLATFORM_H */
